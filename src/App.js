7c139d7d9410b158e8f5c542638137e6355583fb
ianbuen <ianbuen@gmail.com>
2023-02-13 15:47:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AdviceCard from './components/AdviceCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <AdviceCard />
+      </main>
+
+      <footer>
+          <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="https://frontendmentor.io/profile/ianbuen" rel="noreferrer" target="_blank">@ianbuen</a>.
+          </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/AdviceCard.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./AdviceCard.css";
+
+export const AdviceCard = () => {
+
+    const [card, setCard] = useState(null);
+
+    useEffect(() => {
+        generateAdvice();
+    }, []);
+
+
+    const generateAdvice = async () => {
+        setCard(null);
+
+        const newAdvice = await fetch('https://api.adviceslip.com/advice')
+                .then(res => res.json())
+                .then(data => data)
+                .catch(error => error);
+
+        setCard(newAdvice.slip);
+    };
+
+
+    const loadAdvice = () => {
+
+        if (card)
+            return <>
+                <p>{`Advice #${card.id}`}</p>
+                <h1>{`"${card.advice}"`}</h1>
+            </>;
+
+        return ( <h1>Loading...</h1> );
+    };
+
+    return (
+        <div className="card">
+            <div className="card-content">
+                {loadAdvice()}
+                <h1 className="divider"><span></span></h1>
+                <input type="button" onClick={generateAdvice} />
+            </div>
+        </div>
+    );
+};
+
+export default AdviceCard;
